c8a7c8d5b8fe5da5288aaac9536c1ce24ed4bce4
linus <cjc867166763@gmail.com>
2024-11-19 08:29:55 +0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+//let myHeading = document.querySelector("h1");
+//myHeading.textContent = "Hello World!";
+
+//add An image switcher
+let myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png")
+        myImage.setAttribute("src", "images/firefox-icon2.png");
+    else
+        myImage.setAttribute("src", "images/firefox-icon.png");
+};
+
+let myButton = document.querySelector("button")
+let myHeading = document.querySelector("h1")
+
+let setUserName = function () {
+    let myName = prompt("请输入你的名字:");
+    if (!myName)
+        setUserName();
+    else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = "Mozilla COOL, " + myName;
+    }
+}
+
+if (!localStorage.getItem("name"))
+    setUserName();
+else {
+    let storedName = localStorage.getItem("name");
+    myHeading.textContent = "Mozilla COOL, " + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
+
+document.addEventListener("mousemove", () => {
+    let element = document.elementFromPoint(event.clientX, event.clientY);
+    if (element) {
+        let color = window.getComputedStyle(element).color;
+        let rgb = color.match(/\d+/g);
+        if (rgb && rgb[0] > 200 && rgb[1] < 50 && rgb[2] < 50) {
+            alert("你的鼠标在红色文字上!");
+        }
+    }
+}
+);
